10622a6d3d4bae73349cd152f4639175ebecfe46
Steve Fitzgerald <steven.fitzgerald@my.csun.edu>
2024-10-28 11:14:37 -0700

    starting

---
+++ mips/binary2int.s
@@ -0,0 +1,42 @@
+
+
+public static int binary2int () {
+
+    int number;
+    int digit;
+
+    digit = bit2int();
+    number=0;
+    for(; digit != -1; ) {
+      number = number * 2 + digit; 
+      digit = bit2int();
+    }
+
+    return number;
+}
+
+// int bit2int();
+//         - reads an ASCII char
+//         - returns 
+//           * 0 if '0'
+//           * 1 if '1'
+//           * -1 otherwise
+public static int bit2int() {
+
+    int value; 
+
+    value = -1;
+
+    mips.read_c();
+    value = mips.retval();
+
+    if ( value == '0')
+       value = 0;
+    else if (value == '1')
+       value = 1;
+    else
+       value = -1;
+
+    return value;
+}
+
