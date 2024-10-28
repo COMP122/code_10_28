1e0fa8deea27706fb88950d762030bf445087311
Steve Fitzgerald <steven.fitzgerald@my.csun.edu>
2024-10-28 12:33:45 -0700

    fixed some stuff

--- mips/binary2int.s
+++ mips/binary2int.s
@@ -1,52 +1,70 @@
-               .text
-               .globl binary2int
-               .globl bit2int
+            .text
+            .globl binary2int
+            .globl bit2int
 
-               .include "include/stack.s"
-               .include "include/syscalls.s"
-               .include "include/subroutine.s"
-
-binary2int:                      #  public static int binary2int () {
-                                 #  
-                                 #      int number;
-                                 #      int digit;
-                                 #  
-                                 #      digit = bit2int();
-                                 #      number=0;
-                                 #      for(; digit != -1; ) {
-                                 #        number = number * 2 + digit; 
-                                 #        digit = bit2int();
-                                 #      }
-                                 #  
-               move $v0, number  #      return number;
-               jr $ra            #  }
+binary2int: nop                             # public static int binary2int () {
+            # v0: {return value}            # 
+            # t0: $t0                       #     int number;
+            # t1: $t1                       #     int digit;
+            # t2: neg_one                   #     int neg_one;
 
 
+            call bit2int                    #     digit   = bit2int();
+            move $t0, $zero                 #     number  = 0;
+            li $t2, -1                      #     neg_one = -1;
+                          
+
+init:       nop                             #      ;
+loop:       beq $t1, $t2, done              #      for(; digit != neg_one ;) {
+
+body:         nop                           #        ;
+              sll $t0, $t0, 1               #        number = number << 1;
+              add $t0, $t0, $t1             #        number = number + digit; 
+
+next:         nop                           #        ;     
+              call bit2int                  #        digit = bit2int();
+              move $t1, $v0                 #      }
+
+done:         nop                           #      ;
+                                            # 
+              move $v0, $t0                 #  return number;
+              jr $ra
+                                            #  }                                       # 
 
 
 
-                                 # // int bit2int();
-                                 # //         - reads an ASCII char
-                                 # //         - returns 
-                                 # //           * 0 if '0'
-                                 # //           * 1 if '1'
-                                 # //           * -1 otherwise
-bit2int:  nop                    # public static int bit2int() {
-                                 # 
-                                 #     int value; 
-                                 # 
-                                 #     value = -1;
-                                 # 
-                                 #     mips.read_c();
-                                 #     value = mips.retval();
-                                 # 
-                                 #     if ( value == '0')
-                                 #        value = 0;
-                                 #     else if (value == '1')
-                                 #        value = 1;
-                                 #     else
-                                 #        value = -1;
-                                 # 
-            move $v0, value      #     return value;
-            jr $ra               # }
+
+# // int bit2int();
+# //         - reads an ASCII char
+# //         - returns 
+# //           * 0 if '0'
+# //           * 1 if '1'
+# //           * -1 otherwise
+bit2int:    nop                             # public static int bit2int() {
+                                            # 
+                                            #     int value; 
+                                            # 
+                                            #     value = -1;
+                                            # 
+                                            #     mips.read_c();
+                                            #     value = mips.retval();
+                                            # 
+                                            #     switch (value) {
+                                            #        case 0:   mips.next("1");
+                                            #                  value = 0;
+                                            #                  break;
+                                            #                  mips.merge("1");
+                                            # 
+                                            #        case 1:   mips.next("default");
+                                            #                  value = 1;
+                                            #                  break;
+                                            #                  mips.merge("default");
+                                            # 
+                                            #        default:  mips.next();
+                                            #                  value = -1;
+                                            #                  break;
+                                            #     }
+                                            # 
+                                            #     return value;
+                                            # }
 
